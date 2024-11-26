7c45ff1923d12f5b17824590757dabcafed1261c
annwe1 <nann3.carol@gmail.com>
2024-11-26 13:51:44 -0300

    primeiro commit

--- app/src/main/java/br/edu/up/rgm34123768/data/Item.kt
+++ app/src/main/java/br/edu/up/rgm34123768/data/Item.kt
@@ -16,16 +16,11 @@
 
 package br.edu.up.rgm34123768.data
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
 
 /**
  * Entity data class represents a single row in the database.
  */
-@Entity(tableName = "items")
-data class Item(
-    @PrimaryKey(autoGenerate = true)
+class Item(
     val id: Int = 0,
     val name: String,
     val price: Double,
